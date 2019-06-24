b5c00bdb770d4db7246867d4d2a6a13e23d89ff2
leon <315685499@qq.com>
2019-06-24 10:21:48 +0800

    splitChunkPlugins配置详解

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -92,7 +92,25 @@ module.exports = {
     ],
     optimization: {
         splitChunks: {
-            chunks: "all"
+            chunks: "all",
+            minSize: 30000,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+                vendors:{
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                    // name:'vendors.js'
+                },
+                default: {
+                    priority: -20,
+                    reuseExistingChunk: true,
+                    name:'common.js'
+                }
+            }
         }
     },
     output: {
--- src/index.js
+++ src/index.js
@@ -4,19 +4,28 @@ import _ from  'lodash'
 console.log(_.join(['a','d','c'],'*'));
 console.log(_.join(['a','b','c'],'*'));*/
 
-function getComponent() {
-    return import('lodash').then(({default:_}) =>{
-        var element = document.createElement('div');
-        element.innerHTML = _.join(['leon','aries'],'-');
-        return element;
-    })
-}
+import _ from  'lodash'
+import jquery from  'jquery'
 
-getComponent().then(element => {
-    document
-})
+var element = document.createElement('div');
+element.innerHTML = _.join(['leon','aries'],'-');
+document.body.appendChild(element);
+
+
+// function getComponent() {
+//     return import(/*webpackChunkName:'lodash'*/'lodash').then(({default:_}) =>{
+//         var element = document.createElement('div');
+//         element.innerHTML = _.join(['leon','aries'],'-');
+//         return element;
+//     })
+// }
+//
+// getComponent().then(element => {
+//     document.body.appendChild(element)
+// });
+
 
 //代码分割，和webpack无关
 //webpack中实现代码分割，两种方式
 //1.同步加载代码：只需要在webpack.base.conf.js中配置 optimization的配置即可
-//2.异步加载代码（import)：异步代码，无需做任何配置，会自动进行代码分割，需要使用 babel-plugin-dynamic-import-webpack npm包
+//2.异步加载代码（import)：异步代码，无需做任何配置，会自动进行代码分割
